8930511739c5f0c59e9967af045c96f6d2e2e593
Cedric Maniquiz <cgmaniquiz@gmail.com>
2017-09-19 23:34:46 -0700

    added uglify, added sourcemaps, added tslint

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,29 @@
 var gulp            = require("gulp");
 var ts              = require("gulp-typescript");
 var sass            = require('gulp-sass');
+var uglify          = require('gulp-uglify');
+var sourcemaps      = require('gulp-sourcemaps');
+var tslint          = require("gulp-tslint");
 var browserSync     = require('browser-sync').create();
 var tsProject       = ts.createProject("tsconfig.json");
 
 gulp.task('ts', function(){
     return tsProject.src()
     .pipe(tsProject())
-    .js.pipe(gulp.dest("dist"));
+    .js
+    .pipe(sourcemaps.init())
+    .pipe(uglify())
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest("dist"));
 });
+
+gulp.task("lint", () =>
+    gulp.src("src/*.ts")
+        .pipe(tslint({
+            formatter: "stylish"
+        }))
+        .pipe(tslint.report())
+);
 
 // Compile sass into CSS & auto-inject into browsers
 gulp.task('sass', function() {
@@ -25,9 +40,9 @@ gulp.task('serve', function() {
             server: "./dist"
         });
         
-        gulp.watch("src/*.ts", ['ts']).on('change', browserSync.reload);
+        gulp.watch("src/*.ts", ['lint', 'ts']).on('change', browserSync.reload);
         gulp.watch("src/*.scss", ['sass']).on('change', browserSync.reload);
         gulp.watch("dist/*.html").on('change', browserSync.reload);
     });
 
-gulp.task('default', ['sass', 'ts', 'serve']);
+gulp.task('default', ['sass', 'lint', 'ts', 'serve']);
